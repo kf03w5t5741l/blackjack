0d8add01fb4699030a630cc9a089d73e449ea019
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-02-05 14:23:36 +0100

    Refactor PlayerManager pg to PlayerManager pm

--- src/blackjack/Main.java
+++ src/blackjack/Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
         Greeter.printWelcome();
 
-        PlayerManager pg = new PlayerManager();
-        pg.gather();
+        PlayerManager pm = new PlayerManager();
+        pm.gather();
 
-        RoundManager rm = new RoundManager(pg.getPlayers());
+        RoundManager rm = new RoundManager(pm.getPlayers());
         rm.run();
 
-        pg.printHighScores();
+        pm.printHighScores();
 
         Greeter.printFarewell();
     }
